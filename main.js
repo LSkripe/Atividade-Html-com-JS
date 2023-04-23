0d402e1103dc30ba312ce0bbf2207b819cf2f486
Leonardo Skripe <leonardosks10@gmail.com>
2023-04-23 13:22:09 -0300

    alteração de codigo

--- main.js
+++ main.js
@@ -12,15 +12,17 @@ form.addEventListener('submit', function(e) {
     const nomeBeneficiario = document.getElementById('nome-beneficiario');
     const numeroContaBeneficiario = document.getElementById('numero-conta');
     const valorDeposito = document.getElementById('valor-deposito');
-    const mensagemSucesso = 'Montante de: ${valorDepositado.value} depositado para o cliente: ${nomeBeneficiario.value} - conta ${numeroContaBeneficiario}'
+    const mensagemSucesso = 'Montante de: </b>${valorDepositado.value}</b> depositado para o cliente: </b>${nomeBeneficiario.value}<b/> - conta </b>${numeroContaBeneficiario}<b/>';
 
     formEValido = validaNome(nomeBeneficiario.value)
     if (formEValido) {
-        alert(mensagemSucesso);
+        const containerMensagemSucesso = document.querySelector('.success-mensage');
+        containerMensagemSucesso.innerHTML = mensagemSucesso;
+        containerMensagemSucesso.style.display = 'block';
 
-        nomeBeneficiario.value = ' ';
-        numeroContaBeneficiario.value = ' ';
-        valorDeposito.value = ' ';
+        nomeBeneficiario.value = '';
+        numeroContaBeneficiario.value = '';
+        valorDeposito.value = '';
     } else {
         alert("O nome não está completo");
     }
